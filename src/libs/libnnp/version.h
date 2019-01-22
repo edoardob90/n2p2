ec764914c9945988457844ed2a9433aa33d7daf0
edoardob90 <edoardob90@gmail.com>
2019-01-22 16:51:34 +0100

    Using CMake

---
+++ src/libs/libnnp/version.h
@@ -0,0 +1,25 @@
+// n2p2 - A neural network potential package
+// Copyright (C) 2018 Andreas Singraber (University of Vienna)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define NNP_VERSION "2.0.0"
+#define NNP_GIT_REV "143e4df193474ea5d3c64d6dd7792d45bdd15f4f"
+#define NNP_GIT_REV_SHORT "143e4df"
+#define NNP_GIT_BRANCH "master"
+
+#endif
